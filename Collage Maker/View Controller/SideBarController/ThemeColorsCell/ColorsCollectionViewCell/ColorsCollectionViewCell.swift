a7289a833033533baf6797c7d4532a2797270bce
Naveed Khalid <naveedkhalid2030@gmail.com>
2025-01-14 20:15:31 +0500

    Side bar screen completed

---
+++ Collage Maker/View Controller/SideBarController/ThemeColorsCell/ColorsCollectionViewCell/ColorsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ColorsCollectionViewCell.swift
+//  Collage Maker
+//
+//  Created by Naveed Khalid on 14/01/2025.
+//
+
+import UIKit
+
+class ColorsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Collage Maker/View Controller/SideBarController/ThemeColorsCell/ThemeColorsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeColorsCell.swift
+//  Collage Maker
+//
+//  Created by Naveed Khalid on 14/01/2025.
+//
+
+import UIKit
+
+class ThemeColorsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
