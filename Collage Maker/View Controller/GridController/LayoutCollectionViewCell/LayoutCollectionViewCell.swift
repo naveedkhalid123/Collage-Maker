1928a7b4805a40c928ceaac16d69aaa5fbdfef17
Naveed Khalid <naveedkhalid2030@gmail.com>
2025-01-16 16:13:26 +0500

    Grid Controller completed

---
+++ Collage Maker/View Controller/GridController/LayoutCollectionViewCell/LayoutCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LayoutCollectionViewCell.swift
+//  Collage Maker
+//
+//  Created by Naveed Khalid on 16/01/2025.
+//
+
+import UIKit
+
+class LayoutCollectionViewCell: UICollectionViewCell {
+    
+}
